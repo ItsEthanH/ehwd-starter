242bcfc0c86505250a5cf09c8a7f25bfdd9c7bde
Ethan Hawes <ethanhawes@outlook.com>
2022-12-12 14:59:18 +0000

    added 11ty image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,50 @@
+// imports
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
+
+// creates a shortcode for responsive, optimised images.
+async function imageShortcode(src, alt, className, loading, sizes = '(max-width: 64em) 400px, 600px') {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
+  // use the 11ty image plugin to generate a set of images using defined widths.
+  let metadata = await Image(src, {
+    widths: [250, 400, 600],
+    formats: ['webp', 'jpeg'],
+    urlPath: '/images/',
+    outputDir: './public/images',
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
+    },
+  });
+
+  // get the lowest and highest res images
+  let lowsrc = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  // return a picture element with a source for every width defined in metadata
+  return `<picture class="${className}">
+    ${Object.values(metadata)
+      .map((imageFormat) => {
+        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
+          .map((entry) => entry.srcset)
+          .join(', ')}" sizes="${sizes}">`;
+      })
+      .join('\n')}
+      <img
+        src="${lowsrc.url}"
+        width="${highsrc.width}"
+        height="${highsrc.height}"
+        alt="${alt}"
+        loading="${loading}"
+        decoding="async">
+    </picture>`;
+}
+
 module.exports = function (eleventyConfig) {
   // allow assets to be passed into /public
   eleventyConfig.addPassthroughCopy('./src/assets');
@@ -6,6 +53,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     files: './public/css/**/*.css',
   });
+
+  // allows the {% image %} shortcode to be used for optimised iamges (in webp if possible)
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
   return {
     dir: {
